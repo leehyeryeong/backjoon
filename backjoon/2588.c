cb6e2d6aa047fdecad22ef174e5a7fa5e8b825c0
leehyeryeong <w2108@e-mirim.hs.kr>
2022-09-11 23:59:19 +0900

    세자리수의 곱셈

---
+++ backjoon/2588.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	scanf("%d\n", &a);
+	scanf("%d", &b);
+
+	printf("%d\n", a * (b % 10));
+	printf("%d\n", a * (b % 100 / 10));
+	printf("%d\n", a * (b / 100));
+	printf("%d", a * b);
+
+	return 0;
+}
