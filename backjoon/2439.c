26d60293ae8b17c99f7502036dbe0003f3a91409
leehyeryeong <w2108@e-mirim.hs.kr>
2022-09-19 23:48:24 +0900

    오른쪽 정렬 별찍기 수정

--- backjoon/2439.c
+++ backjoon/2439.c
@@ -6,10 +6,10 @@ int main() {
 	scanf("%d", &n);
 
 	for (int i = 0; i < n; i++) {
-		for (int j = 1; j < n - i; j++) {
+		for (int j = 0; j < n - i; j++) {
 			printf(" ");
 		}
-		for (int k = 0; k < i + 1; k++) {
+		for (int k = 0; k <= i; k++) {
 			printf("*");
 		}
 		printf("\n");
