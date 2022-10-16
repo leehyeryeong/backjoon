115a4c2901c6156ec89ccbf4496e29168e6ae7d9
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-16 23:56:14 +0900

    이익이 발생하는 지점을 찾는 문제

---
+++ backjoon/1712.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+
+	if (c <= b) {
+		printf("-1");
+	}
+	else {
+		printf("%d", a / (c - b) + 1);
+	}
+
+	return 0;
+}
